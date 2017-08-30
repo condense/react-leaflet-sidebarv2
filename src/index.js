5a3cfb0ace0835597f40e6620fe31863e8cc1e09
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-08-30 11:24:10 +1000

    Ejecting, and turning into a library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Sidebar, Tab } from './Sidebar';
-import registerServiceWorker from './registerServiceWorker';
-import SidebarExample from './sidebar-example'
-
-ReactDOM.render(<SidebarExample />, document.getElementById('root'));
-registerServiceWorker();
+export { Sidebar, Tab } from './Sidebar';
